8e87425441d087c691fefabb4a0541965932968b
John Letey <john@kyve.network>
2021-12-01 12:14:42 +0000

    chore: remove description

--- dist/src/utils/index.js
+++ dist/src/utils/index.js
@@ -25,7 +25,7 @@ class CLI extends commander_1.Command {
         this.option("-e, --endpoint <string>", "A custom Moonbase Alpha endpoint. [optional]");
         this.option("-g, --gas-multiplier <string>", "The amount that you want to multiply the default gas price by. [optional]");
         this.option("-st, --send-statistics <boolean>", "Send statistics. [optional, default = true]", true);
-        this.option("--verbose", "[optional, default = false]", false);
+        this.option("--verbose", undefined, false);
         this.version(packageVersion, "-v, --version");
     }
 }
