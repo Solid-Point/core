6b9a141b5daa582a62134a6c45358bba3946b4e4
Troy Kessler <troy.kessler99@gmail.com>
2022-04-07 16:31:36 +0200

    changed for video

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,7 +30,7 @@ export class CLI extends Command {
     this.option(
       "-n, --network <string>",
       "The chain id of the network. [optional, default = korellia]",
-      "korellia"
+      "alpha"
     );
     this.option(
       "-sp, --space <number>",
